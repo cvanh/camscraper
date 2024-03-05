a77d2b9943de476f1bf665f51b09252ea69e6d0f
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2024-03-05 10:11:44 +0100

    setup of api

---
+++ backend/src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/')]
+    public function home(): Response
+    {
+        return $this->render('home.twig',
+            [
+                'data' => 1,
+            ]
+        );
+    }
+}
