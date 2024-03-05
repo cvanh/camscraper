15860d883fa1d5dd661b17281ec3819117ffda84
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2024-03-05 17:36:50 +0100

    wip on klimwinkel scraper testing

--- scrapers/klimwinkel/klimwinkel_test.go
+++ scrapers/klimwinkel/klimwinkel_test.go
@@ -1,12 +1,21 @@
 package main
 
-import "testing"
+import (
+	"testing"
 
-type mockhttpclient struct {}
+	"github.com/jarcoal/httpmock"
+)
 
-func getproductTest(t *testing.T){
-	f := getProduct(69)
+func TestGetproduct(t *testing.T){
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
 
+	httpmock.RegisterResponder("GET", "https://www.klimwinkel.nl/api/products/69",
+    httpmock.NewStringResponder(200, `[{"id": 1, "name": "My Great Article"}]`))
 
+	httpmock.GetTotalCallCount()
 
+	f := getProduct(69);
+
+	t.Logf("%v",f)
 }
